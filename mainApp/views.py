7be076a77e30ed41014e965fb29b90431d324fd1
Richard <richardwesselsofficial@gmail.com>
2024-04-29 21:21:46 +0200

    Get text endpoint accepts difficulty and filters texts returned accordingly. At the moment, difficulty filtering is done by simply seeing if prompt used a2, b1 or b2.

--- mainApp/views.py
+++ mainApp/views.py
@@ -6,6 +6,26 @@ from mainApp.serializers import LLMTextWithTranslationSerializer, LanguageSerial
 
 import random
 
+
+def confirm_difficulty(difficulty: str, text: LLMTextWithTranslation):
+    '''
+    Checks difficulty by looking at prompt text. Used with `filter` to allow only texts of desired difficulty.
+    '''
+    text_difficulty = "Normal"
+    text_prompt: str = text.original_prompt.lower()
+
+    if "a2" in text_prompt:
+        text_difficulty = "Easy"
+
+    if "b1" in text_prompt:
+        text_difficulty = "Normal"
+
+    if "b2" in text_prompt:
+        text_difficulty = "Difficult"
+    
+    return difficulty == text_difficulty
+
+
 @api_view(['GET'])
 def get_all_texts(request):
     all_texts = LLMTextWithTranslation.objects.all()
@@ -13,9 +33,11 @@ def get_all_texts(request):
     return Response(serializer.data)
 
 @api_view(['GET'])
-def get_text_of_language(request, language):
+def get_text_of_language(request, language, difficulty):
     language_fk = Language.objects.get(language_name=language)
     language_texts = list(LLMTextWithTranslation.objects.filter(language=language_fk))
+    # filter to only allow texts of specified difficulty
+    language_texts = list(filter(lambda text: confirm_difficulty(difficulty, text), language_texts))
     item = random.choice(language_texts)
     serializer = LLMTextWithTranslationSerializer(item, many=False, context={'request': request})
     return Response(serializer.data)
