5fa8364f99754e8cc595a15034babfaf6215c9ef
Richard <richardwesselsofficial@gmail.com>
2026-05-10 10:22:44 +0200

    Add DRF Spectacular for OpenAPI schema generation.

--- backend/dopios_backend/settings.py
+++ backend/dopios_backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "django_filters",
+    "drf_spectacular",
     "mainApp",
 ]
 
@@ -72,6 +73,10 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
 
 WSGI_APPLICATION = "dopios_backend.wsgi.application"
 
