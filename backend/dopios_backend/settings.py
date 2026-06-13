646b2ab28ab126309eef9328f398fdf4d929b83a
Richard <richardwesselsofficial@gmail.com>
2026-06-13 17:33:23 +0200

    Change owner of `/app/staticfiles` and bind it to a volume to fix permission error when running `collectstatic`.

--- backend/dopios_backend/settings.py
+++ backend/dopios_backend/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-SECRET_KEY = env("DJANGO_SECRET_KEY")
+SECRET_KEY = env.str("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG")
