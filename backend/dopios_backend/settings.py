ff4fbad142c581791f3e1faa96cd764156c3badd
Richard <richardwesselsofficial@gmail.com>
2026-07-12 15:46:56 +0200

    Update ALLOWD_HOSTS to be provided by env variable.

--- backend/dopios_backend/settings.py
+++ backend/dopios_backend/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = env.str("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG")
 
-ALLOWED_HOSTS = ["penguinos.autitique.com"]
+ALLOWED_HOSTS = env.list(
+    "DJANGO_ALLOWED_HOSTS",
+)
 
 
 # Application definition
